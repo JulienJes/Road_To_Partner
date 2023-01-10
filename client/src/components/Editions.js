22ddd711cb8be4a683a9a2e97b7d90193a54c538
Julien Jesionek <julien.jesionek@gmail.com>
2023-01-10 13:51:32 +0100

    animations

--- client/src/components/Editions.js
+++ client/src/components/Editions.js
@@ -5,19 +5,7 @@ function Editions () {
         <>
         <section className="editions">
             <div className="list">
-                <div className="card">
-                    <div className="card-title">
-                        <h3>RTP#1 - PUBG</h3>
-                    </div>
-                    <div className="card-dates">
-                        <p>Jeudi 17 novembre 2022 à 20h00</p>
-                        <p>Fin des inscriptions le 14 novembre inclus</p>
-                    </div>
-                    <div className="card-btn">
-                        <NavLink exact="true" to="/rtp1" className="btn">Consulter</NavLink>
-                    </div>
-                </div>
-                <div className="card">
+                <div className="card fadein--1">
                     <div className="card-title">
                         <h3>RTP#2 - Fall Guys</h3>
                     </div>
@@ -27,6 +15,18 @@ function Editions () {
                     </div>
                     <div className="card-btn">
                         <NavLink exact="true" to="/rtp2" className="btn">Consulter</NavLink>
+                    </div>
+                </div>
+                <div className="card fadein--2">
+                    <div className="card-title">
+                        <h3>RTP#1 - PUBG</h3>
+                    </div>
+                    <div className="card-dates">
+                        <p>Jeudi 17 novembre 2022 à 20h00</p>
+                        <p className="inscriptions-over">Évènement terminé</p>
+                    </div>
+                    <div className="card-btn">
+                        <NavLink exact="true" to="/rtp1" className="btn">Consulter</NavLink>
                     </div>
                 </div>
             </div>
