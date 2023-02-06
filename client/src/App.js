eb207c3331aa85904b649289d62b8e9b827e8b0d
Julien Jesionek <julien.jesionek@gmail.com>
2023-02-06 12:50:02 +0100

    API TWITCH

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import Footer from './components/footer/Footer';
 //     console.log(result)
 //   })
 
+
+
 function App() {
   return (
     <>
---
+++ client/src/config/Twitch.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+function TwitchAPI() {
+    const [data, setData] = useState([]);
+
+useEffect(() => {
+    const headers = new Headers({
+      'Authorization': `Bearer ${process.env.TOKEN_API_TWITCH}`
+    });
+
+    fetch('https://api.twitch.tv/helix/streams', { headers })
+      .then(res => res.json())
+      .then(res => setData(res.data))
+      .catch(err => console.error(err));
+  }, []);
+};
+
+export default TwitchAPI;
