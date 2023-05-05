29d44a1ca51b6049dd70f518463854b7ce150aaf
Julien Jesionek <julien.jesionek@gmail.com>
2023-05-05 02:40:20 +0200

    logo après redirection

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -9,7 +9,7 @@ function Header () {
             </div>
             <div className="logo-container">
                 <NavLink exact="true" to="/">
-                    <img src="./img/logo/logo.webp" alt="logo" key={Date.now()}/> { /* key={Date.now()} permet de forcer le rechargement de l'image */ }
+                    <img src="/img/logo/logo.webp" alt="logo" key={Date.now()}/> { /* key={Date.now()} permet de forcer le rechargement de l'image */ }
                     <h1>Road To Partner</h1>
                 </NavLink>
             </div>
