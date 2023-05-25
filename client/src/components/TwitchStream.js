9906f3b3b14f2b002d3f30d8fb366492ddc36c49
Julien Jesionek <julien.jesionek@gmail.com>
2023-05-26 00:03:40 +0200

    streams ongoing

--- client/src/components/TwitchStream.js
+++ client/src/components/TwitchStream.js
@@ -1,9 +1,9 @@
 const TwitchStream = ({ channel }) => (
     <iframe
-        src={`https://player.twitch.tv/?channel=${channel}&parent=http://localhost:3000/`}
-        height="720"
-        width="1280"
-        allowfullscreen="true"
+        src={`https://player.twitch.tv/?channel=${channel}&parent=localhost`}
+        height="200"
+        width="354"
+        allowFullScreen
         title={`Live de ${channel}`}
     ></iframe>
 )
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,25 +5,34 @@ import TwitchStream from "../components/TwitchStream"
 function Home() {
     return (
         <>
-            <section className="home">
-                <div className="title">
-                    <h2>Bienvenue !</h2>
-                </div>
-                <div className="catchphrase">
-                    <p>
-                        Road To Partner ou RTP pour les intimes est une
-                        compétition sur Twitch où un maximum de 100 streamers
-                        s'affrontent, le tout casté par JowyTV. Tous les
-                        participants doivent s'abonner et raid la chaîne Twitch
-                        du vainqueur d'une édition. <br />
-                        RTP est destiné aux streameurs affiliés qui ambitionnent
-                        de passer Partner Twitch.
-                    </p>
-                </div>
-            </section>
-            <Editions />
+            <div>
+                <section className="home">
+                    <div className="title">
+                        <h2>Bienvenue !</h2>
+                    </div>
+                    <div className="catchphrase">
+                        <p>
+                            Road To Partner ou RTP pour les intimes est une
+                            compétition sur Twitch où un maximum de 100
+                            streamers s'affrontent, le tout casté par JowyTV.
+                            Tous les participants doivent s'abonner et raid la
+                            chaîne Twitch du vainqueur d'une édition. <br />
+                            RTP est destiné aux streameurs affiliés qui
+                            ambitionnent de passer Partner Twitch.
+                        </p>
+                    </div>
+                </section>
+                <Editions />
+            </div>
             <aside>
-                <TwitchStream channel="JowyTV" />
+                <div>
+                    <div>
+                        <h3>Ces participants sont en live!</h3>
+                    </div>
+                    <div>
+                        <TwitchStream channel="Sovannya" />
+                    </div>
+                </div>
             </aside>
         </>
     )
