0355295c894994704e61119067590fbfc2244f6e
Julien Jesionek <julien.jesionek@gmail.com>
2023-05-25 15:30:12 +0200

    TwitchStream ongoing

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,32 @@
 //import { NavLink } from "react-router-dom";
-import Editions from "../components/Editions";
+import Editions from "../components/Editions"
+import TwitchStream from "../components/TwitchStream"
 
-function Home () {
+function Home() {
     return (
         <>
-        <section className="home">
-            <div className="title">
-                <h2>Bienvenue !</h2>
-            </div>
-            <div className="catchphrase">
-                <p>Road To Partner ou RTP pour les intimes est une compétition sur Twitch où un maximum de 100 streamers s'affrontent, le tout casté par JowyTV. Tous les participants doivent s'abonner et raid la chaîne Twitch du vainqueur d'une édition. <br/>
-                RTP est destiné aux streameurs affiliés qui ambitionnent de passer Partner Twitch.</p>
-            </div>
-        </section>
-        <Editions />
+            <section className="home">
+                <div className="title">
+                    <h2>Bienvenue !</h2>
+                </div>
+                <div className="catchphrase">
+                    <p>
+                        Road To Partner ou RTP pour les intimes est une
+                        compétition sur Twitch où un maximum de 100 streamers
+                        s'affrontent, le tout casté par JowyTV. Tous les
+                        participants doivent s'abonner et raid la chaîne Twitch
+                        du vainqueur d'une édition. <br />
+                        RTP est destiné aux streameurs affiliés qui ambitionnent
+                        de passer Partner Twitch.
+                    </p>
+                </div>
+            </section>
+            <Editions />
+            <aside>
+                <TwitchStream channel="JowyTV" />
+            </aside>
         </>
-    );
-};
+    )
+}
 
-export default Home;
+export default Home
